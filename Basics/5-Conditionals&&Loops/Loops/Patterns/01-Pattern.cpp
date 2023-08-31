3d1c6ba6224aff50b237c7a41065a5eada09b40a
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-08-31 13:16:45 +0530

    cls 05 | Patterns 01 and 02

---
+++ Basics/5-Conditionals&&Loops/Loops/Patterns/01-Pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+/**
+  Patter 01
+
+  * * * *
+  * * * *
+  * * * *
+  * * * *
+*/
+int main() {
+
+  for (int i = 0; i < 4; i++) {
+
+    for (int j = 0; j < 4; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+    return 0;
+}
+
+/*
+  OUTPUT
+
+  * * * *
+  * * * *
+  * * * *
+  * * * *
+*/
