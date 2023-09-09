fcbcb9f3d8af6a652d99a85d2c91f0f1ed285318
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-09-09 10:02:07 +0530

    🥂Array creation and basics

---
+++ DataStructure/Arrays/basics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+  
+  // Array creation
+  int arr[10];
+  char ch[13];
+  bool flag[4];
+
+  // Access address
+  cout << "Address of arr: " << &arr << endl; //Address of arr: 0xfad33ff6b0
+
+  return 0;
+}
