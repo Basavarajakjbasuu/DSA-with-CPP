cc270b0dfd00d02e1e7f1f76fa32159d594a5a0b
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-10-02 14:57:44 +0530

    decode message 🙈🙈

---
+++ DataStructure/Arrays/Strings/05-decodeMessage.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+ 
+string decodeMessage(string key, string message) {
+  // create Mapping
+  char start = 'a';
+  char mapping[260] = {0};
+
+  for(auto ch: key) {
+      if(ch != ' ' && mapping[ch] == 0) {
+          mapping[ch] = start;
+          start++;
+      }
+  }
+
+  // use mapping
+  string ans;
+  for(auto ch: message) {
+      if(ch == ' ') {
+          ans.push_back(' ');
+      }else {
+          char decodedChar = mapping[ch];
+          ans.push_back(decodedChar);
+      }
+  }
+
+  return ans;
+} 
+int main() {
+
+  string key = "the quick brown fox jumps over the lazy dog";
+  string message = "vkbs bs t suepuv";
+
+  cout << decodeMessage(key, message) << endl;
+
+  return 0;
+}
