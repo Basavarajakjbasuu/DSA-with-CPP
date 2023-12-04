32a4dcf91f3f88b79cce787bcc9ebe7f3afe2071
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-12-04 10:50:13 +0530

    level order traverse with marker

--- 11-Tree/02-levelOrderTraverse.cpp
+++ 11-Tree/02-levelOrderTraverse.cpp
@@ -58,17 +58,60 @@ void levelOrderTraverse(Node *root) {
   
 }
 
+
+
+void levelOrderTraverseWithMarker(Node *root) {
+  queue<Node*> qu;
+  qu.push(root);
+  qu.push(NULL);
+
+  //if we take qu.empty() it will go infinite loop at last node because every time adding null so 
+  while (qu.size() > 1)
+  {
+    Node* front = qu.front();
+    qu.pop();
+
+    if(front == NULL) {
+      cout << endl;
+      qu.push(NULL);
+    }else {
+      // valid
+      cout << front->data << " ";
+
+      if(front->left != NULL) {
+        qu.push(front->left);
+      }
+
+      if(front->right != NULL) {
+        qu.push(front->right);
+      }
+    }
+
+  }
+  
+}
+
 int main() {
   cout << "Enter the value for Node: " << endl;
   Node *root = createTree();
 
   cout<<"Level order: ";
   levelOrderTraverse(root);
+
+  cout << endl;
+
+  cout << "Level order with Marker: " << endl;
+  levelOrderTraverseWithMarker(root);
   return 0;
 }
+
 
 /*
   Enter the value for Node: 
   10 20 40 -1 -1 -1 30 50 -1 -1 60 -1 -1
-  Level order: 10 20 30 40 50 60
+  Level order: 10 20 30 40 50 60 
+  Level order with Marker: 
+  10 
+  20 30 
+  40 50 60 
 */
