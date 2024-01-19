d89c7ca4b3f59a171238aba56b819fb4f39b19bb
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-01-19 10:59:53 +0530

    BST traversal

--- 12-BST/00-basics.cpp
+++ 12-BST/00-basics.cpp
@@ -15,20 +15,6 @@ class Node{
   }
 };
 
-//should pass by reference
-void createBST(Node* &root) {
-  cout << "Enter data: ";
-  int data;
-  cin>> data;
-
-  while(data!=-1) {
-    cout << "Enter data: ";
-    root = insertIntoBST(root, data);
-    cin>> data;
-  }
-
-}
-
 Node* insertIntoBST(Node* root, int data) {
   //BASE CASE
   if(root == NULL) {
@@ -45,6 +31,22 @@ Node* insertIntoBST(Node* root, int data) {
 
   return root;
 }
+
+
+//should pass by reference
+void createBST(Node* &root) {
+  cout << "Enter data: ";
+  int data;
+  cin>> data;
+
+  while(data!=-1) {
+    cout << "Enter data: ";
+    root = insertIntoBST(root, data);
+    cin>> data;
+  }
+
+}
+
 
 void levelOrderTraversal(Node* root) {
   queue<Node*>  q;
@@ -73,26 +75,63 @@ void levelOrderTraversal(Node* root) {
 }
 
 
+void preOrder(Node* root) {
+  if(root == NULL) 
+    return;
+
+  //NLR
+  cout<<root->data << " ";
+  preOrder(root->left);
+  preOrder(root->right);
+}
+
+void inOrder(Node* root) {
+  if(root == NULL)
+    return;
+
+  //LNR
+  inOrder(root->left);
+  cout << root->data << " ";
+  inOrder(root->right);
+}
+
+void postOrder(Node* root) {
+  if(root == NULL)
+    return;
+
+  //LRN
+  postOrder(root->left);
+  postOrder(root->right);
+  cout << root->data << " ";
+}
+
 int main() {
   Node* root = NULL;
   createBST(root);
   levelOrderTraversal(root);
+
+  cout << endl << "InOrder: "; inOrder(root);
+  cout << endl << "PostOrder: "; postOrder(root);
+  cout << endl << "PreOrder: "; preOrder(root);
   return 0;
 }
 
 /*
   Enter data: 50
   Enter data: 30
-  Enter data: 20
+  Enter data: 60
   Enter data: 25
   Enter data: 40
-  Enter data: 60
   Enter data: 70
   Enter data: 80
   Enter data: 55
   Enter data: -1
   50
   30 60
-  20 40 55 70
-  25 80
+  25 40 55 70
+  80
+
+  InOrder: 25 30 40 50 55 60 70 80 
+  PostOrder: 25 40 30 55 80 70 60 50 
+  PreOrder: 50 30 25 40 60 55 70 80
 */
