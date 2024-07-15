783ddbb219986e0bff832b2c89331d3584377101
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-07-15 10:17:04 +0530

    Find missing element with duplicate by SORTING and SWAPPING method

--- 01-Arrays/100-Assignments/04-find-missing-element-duplicates.cpp
+++ 01-Arrays/100-Assignments/04-find-missing-element-duplicates.cpp
@@ -20,13 +20,46 @@ void findMissing(int a[], int N) {
       cout << "Missing element: " <<i+1;
     }
   }
-  
+}
+
+// SORTING and SWAPPING method
+void findMissing2(int a[], int N) {
+
+  int i=0;
+  while (i<N) {
+    int index = a[i] - 1;
+    if(a[i] != a[index]) {
+      swap(a[i], a[index]);
+    } else {
+      i++;
+    }
+  }
+
+  // for(int i=0; i<N; i++) {
+  //   cout << a[i] << " ";
+  // }
+
+  for(int i=0; i<N; i++) {
+
+    if(a[i] != i+1) {
+      cout << "Missing element: " << i+1 << " ";
+    }
+  }
 }
 
 int main() {
-  int a[5] = {1,3,5,3,4};
+  int a[5] = {1,3,3,3,4};
+  int a1[5] = {1,3,3,3,5};
 
   findMissing(a, 5);
+  cout << endl;
+  findMissing2(a1, 5);
 
   return 0;
 }
+
+/*
+  -1 3 -3 -3 4 
+  Missing element: 2Missing element: 5
+  Missing element: 2 Missing element: 4 
+*/
