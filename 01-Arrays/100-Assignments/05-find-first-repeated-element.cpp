e15a940afd1f7ee6e625aa7f8b3ebb481693d200
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-07-15 10:38:26 +0530

    find first repeated element optimal solution

--- 01-Arrays/100-Assignments/05-find-first-repeated-element.cpp
+++ 01-Arrays/100-Assignments/05-find-first-repeated-element.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 int firstRepeated(int a[], int n) {
@@ -18,12 +19,28 @@ int firstRepeated(int a[], int n) {
   return -1;
 }
 
+int firstRepeatedOptimal(int arr[], int n) {
+   unordered_map<int, int> hash;
+        for(int i=0; i<n; i++) {
+            hash[arr[i]]++;
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(hash[arr[i]] > 1) {
+                return arr[i];
+            }
+        }
+    return -1;    
+}
+
 int main() {
  
   int a[] = {1,5,3,4,3,5,6};
 
   cout << "First repeated element: " << firstRepeated(a, 7) << endl;
+  cout << "First repeated element: " << firstRepeatedOptimal(a, 7) << endl;
   return 0;
 }
 
+// First repeated element: 5
 // First repeated element: 5
