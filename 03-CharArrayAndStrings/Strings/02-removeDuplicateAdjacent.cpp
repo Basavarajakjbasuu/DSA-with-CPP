82cffa447c8aebca8d86fd6301676796f25c8dd5
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-08-03 11:07:02 +0530

    Reworked ⚒️⚒️remove duplicate adjacent string char

--- 03-CharArrayAndStrings/Strings/02-removeDuplicateAdjacent.cpp
+++ 03-CharArrayAndStrings/Strings/02-removeDuplicateAdjacent.cpp
@@ -2,16 +2,18 @@
 #include<string.h>
 
 using namespace std;
+//New string method
 string removeDuplicates(string s) {
     string ans = "";
     int index = 0;
     int len = s.length();
 
     while(index < s.length()) {
-
+        // we will check if current character index and  previous index element(right most element) is same then pop it
         if(ans.length() > 0 && ans[ans.length() - 1] == s[index]) {
             ans.pop_back();
         }else {
+            //else will push it new string
             ans.push_back(s[index]);
         }
         index++;
